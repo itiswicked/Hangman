5bb12eb6fab5f1d40bac937e59218cbbe5c61948
Nathan Bourke <nathanrbourke@gmail.com>
2016-07-21 22:32:37 -0400

    update test file to use slack authorization

--- send_letter.rb
+++ send_letter.rb
@@ -1,10 +1,15 @@
 require 'httparty'
+require 'dotenv'
+
+Dotenv.load
+
 options = {
   body: {
-    guess: ARGV.first
+    token: ENV['INCOMING_SLACK_MESSAGE_TOKEN'],
+    text: ARGV.first
   }
 }
 
 response = HTTParty.post('http://localhost:3000/api/v1/integrations', options)
 
-puts response.body[0..4000]
+puts response.body[0..2000]
